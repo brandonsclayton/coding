8887d49550f9990be58eb1ff6434c20c71c2ffd3
brandonsclayton <bclayton@mines.edu>
2017-06-14 14:36:06 -0600

    modified:   layout.css 	modified:   layout.html 	modified:   layout.js 	modified:   ../light_stat_card/lights.css 	modified:   ../light_stat_card/lights.js 	modified:   ../progress_bar/stacked_progress_bar.html

--- html/layout/layout.js
+++ html/layout/layout.js
@@ -1,14 +1,16 @@
+
+//.................. Set Style Sheet Function .................................. //
 function swap_style(tab)
 {   
-    var temp_style =   "https://code.getmdl.io/1.1.3/material.indigo-red.min.css"   
-    var lights_style = "https://code.getmdl.io/1.3.0/material.purple-green.min.css"
-    var har_style =    "https://code.getmdl.io/1.3.0/material.orange-indigo.min.css"
+  var temp_style =   "https://code.getmdl.io/1.1.3/material.indigo-red.min.css"   
+  var lights_style = "https://code.getmdl.io/1.3.0/material.purple-green.min.css"
+  var har_style =    "https://code.getmdl.io/1.3.0/material.orange-indigo.min.css"
 
-    if      (tab == 'temp')   { var sheet = temp_style;}
-    else if (tab == 'lights') { var sheet = lights_style;}
-    else if (tab == 'harmony'){ var sheet = har_style;}
-    
-    document.getElementById("pagestyle").setAttribute("href", sheet); 
+  if      (tab == 'temp')   { var sheet = temp_style;}
+  else if (tab == 'lights') { var sheet = lights_style;}
+  else if (tab == 'harmony'){ var sheet = har_style;}
+  
+  document.getElementById("pagestyle").setAttribute("href", sheet); 
 }
-
+//------------- End Set Style Sheet Function ---------------------------------- //
 
--- html/light_stat_card/lights.js
+++ html/light_stat_card/lights.js
@@ -1,54 +1,53 @@
 
    
-    
+//.......................... Dimmer Switch Function ......................... //    
 function dimmer(light_stat)
 {
     
-    if (light_stat == 'light_on')
-    {     
-        var slider = document.getElementById('dim').value;
-        var scale = slider/100.0
-        document.getElementById('light_bulb').style = "opacity: " + scale;
-        var style = document.createElement("STYLE");
-        var opa = document.createTextNode(".mdl-slider__background-lower { background: orange;opacity: "+ scale + ";}");
-        style.appendChild(opa);
-        document.head.appendChild(style);
-    }
-    else if (light_stat == 'light_off')
-    {
-        //document.getElementById('dim').MaterialSlider.change(0); 
-        var style = document.createElement("STYLE");
-        var opa = document.createTextNode(".mdl-slider__background-lower { background: grey ; opacity: .55 }");
-        style.appendChild(opa);
-        document.head.appendChild(style);
-
-
-    }
+  if (light_stat == 'light_on')
+  {     
+    var slider = document.getElementById('dim').value;
+    var scale = slider/100.0
+    document.getElementById('light_bulb').style = "opacity: " + scale;
+    var style = document.createElement("STYLE");
+    var opa = document.createTextNode(".mdl-slider__background-lower { background: orange;opacity: "+ scale + ";}");
+    style.appendChild(opa);
+    document.head.appendChild(style);
+  }
+  else if (light_stat == 'light_off')
+  {
+    var style = document.createElement("STYLE");
+    var opa = document.createTextNode(".mdl-slider__background-lower { background: grey ; opacity: .55 }");
+    style.appendChild(opa);
+    document.head.appendChild(style);
+  }
 }
+//--------------------- End Dimmer Switch Function ------------------------- //
 
 
+//..................... Light Switch Function ............................. //
 function light_switch(stat)
 {   
-    var image = document.getElementById('light_bulb');
-    var light_stat = image.src.match('light_on');
-    if (stat == 'light')
-    { 
-        if (image.src.match("light_on")) 
-        {
-            image.src = "Fig/light_off.png";
-            dimmer('light_off');
-        } 
-        else
-        {
-            image.src = "Fig/light_on.png";
-            dimmer('light_on');
-        }      
-    }
-    else if (stat == 'dim')
+  var image = document.getElementById('light_bulb');
+  var light_stat = image.src.match('light_on');
+  if (stat == 'light')
+  { 
+    if (image.src.match("light_on")) 
     {
-        dimmer(light_stat);
-    }
+        image.src = "Fig/light_off.png";
+        dimmer('light_off');
+    } 
+    else
+    {
+        image.src = "Fig/light_on.png";
+        dimmer('light_on');
+    }      
+  }
+  else if (stat == 'dim')
+  {
+    dimmer(light_stat);
+  }
 
 }
-
+//------------- End Light Switch Function ------------------------------- //
 
