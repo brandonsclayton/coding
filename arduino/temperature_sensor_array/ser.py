d01394dd383ec049eb769697f1c0c7d42efeb51a
brandonsclayton <brandonsclayton@gmail.com>
2017-07-08 17:06:56 -0600

    modified:   ../../ser.py 	modified:   ../lib/symlink.bash 	modified:   sketch.ino

--- arduino/temperature_sensor_array/ser.py
+++ arduino/temperature_sensor_array/ser.py
@@ -3,10 +3,8 @@
 import serial
 import time
 
-ser = serial.Serial('/dev/ttyACM0',9600)
+ser = serial.Serial('/dev/ttyACM1',9600)
 
-ihum  = 0       # Index where humidity is
-itemp = 1       # Index where temp is
 
 while(True):
     #start_time = time.time()
--- arduino/temperature_sensor_array/temp_sensor00/src/sketch.ino
+++ arduino/temperature_sensor_array/temp_sensor00/src/sketch.ino
@@ -2,90 +2,80 @@
 //##############################
 //
 //........ Include .............
-#include <SPI.h>
-#include <RH_NRF24.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <DHT.h>
-
+#include <Wire.h>
+#include "Adafruit_MCP9808.h"
 //------- End Include ----------
 //
 //##############################
 
 
-//............... Define ................
-#define DHTPIN A0   
-#define DHTTYPE DHT11 // DHT 11
-//---------------------------------------
 
-//............... Setup .......................
-
-DHT dht(DHTPIN, DHTTYPE);
-
-
-RH_NRF24 nrf24;
-// RH_NRF24 nrf24(8, 10);// For Leonardo, need explicit SS pin
-//---------------------------------------------
+// ................ Setup ......................
+sensor = "temp_sensor00";
+Adafruit_MCP9808 temp_sensor = Adafruit_MCP9808();
+//----------------------------------------------
 
 //............... Variables ....................
-long wait = 400;      // Wait time in seconds per temp reading
+long wait = 1000;      // Wait time in mili seconds per temp reading
 //---------------------------------------------
+
+
 
 void setup() 
 {
-    Serial.begin(9600);
-  
-    //................. Transmitter Setup ...............
-    while (!Serial);                  // wait for serial port to connect. Needed for Leonardo only
-    if (!nrf24.init())
-        Serial.println("init failed");  // Defaults after init are 2.402 GHz (channel 2), 2Mbps, 0dBm
-    if (!nrf24.setChannel(1))
-        Serial.println("setChannel failed");
-    if (!nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm))
-        Serial.println("setRF failed");    
-//-----------------------------------------------
+  Serial.begin(9600);
 
-//.................. Temperature Sensor Setup .................
-    dht.begin();
-//-------------------------------------------------------------
+  /*
+  //................. Transmitter Setup ...............
+  while (!Serial);                  // wait for serial port to connect. Needed for Leonardo only
+  if (!nrf24.init())
+    Serial.println("init failed");  // Defaults after init are 2.402 GHz (channel 2), 2Mbps, 0dBm
+  if (!nrf24.setChannel(1))
+    Serial.println("setChannel failed");
+  if (!nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm))
+    Serial.println("setRF failed");    
+  //-----------------------------------------------
+  */
+
+  //.................. Temperature Sensor Setup .................
+  if (!temp_sensor.begin())
+  {
+    Serial.println("Could not find temperature sensor");
+    while(1);
+  }
+  //-------------------------------------------------------------
 
 }
 
 
 void loop()
 {
-    char temp_str[6];
-    char hum_str[6];
- 	char msg[50];
+  char temp_str[6];                       // String to hold temp 
+ 	char msg[50];                           // String to hold message to send
 
-    float hum = dht.readHumidity();
-    float temp = dht.readTemperature();
-    temp=temp*9/5+32;
-
-// check if returns are valid, if they are NaN (not a number) then something went wrong!
-    if (isnan(temp) || isnan(hum))
-    {
-        Serial.println("Failed to read from DHT");
-    }
-    else
-    {
-        dtostrf(hum,5,2,hum_str);
-        dtostrf(temp,5,2,temp_str);
-        
-        sprintf(msg,"Living Room:%s,%s",temp_str,hum_str);
-    	Serial.println("Sending message:");
-        Serial.println(msg);
-        Serial.print("\n\n");
-
-        nrf24.send((uint8_t*)msg, strlen(msg)+1);
-    	nrf24.waitPacketSent();
-        
-        delay(wait);         // Wait
-    }
-
-
-
+  float temp = temp_sensor.readTempC();   // Read in temperature in C
+  temp=temp*9.0/5.0+32.0;                       // Convert to F
   
+  
+  // check if returns are valid, if they are NaN (not a number) then something went wrong!
+  if (isnan(temp) || isnan(hum))
+  {
+      Serial.println("Failed to read from temp_sensor00");
+  }
+  else
+  {
+    dtostrf(temp,5,2,temp_str);
+  
+    sprintf(msg,"%s: %s",sensor,temp_str);
+    Serial.println("Message: ");
+    Serial.println(msg);
+    Serial.print("\n\n");
+
+    //nrf24.send((uint8_t*)msg, strlen(msg)+1);
+    //nrf24.waitPacketSent();
+      
+    delay(wait);         // Wait
+  }
   
 }
 
